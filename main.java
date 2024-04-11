d2abe51c4581b798e76dfc7715b51984a54d3b30
IsaacMcGill <aaronisaacmcgill@gmail.com>
2024-04-10 19:15:59 -0500

    fullCode

--- main.java
+++ main.java
@@ -1,10 +1,25 @@
 public class main{
         public static void main(String[] args){
-        System.out.println("Hello, World!");
-        branchAnalysis first = new branchAnalysis(0.3);
+        branchAnalysis first = new branchAnalysis(0.1);
         System.out.println(first.getOdds());
-        first.printArray();
+        //first.printArray();
         System.out.println(first.singlePredictor());
         System.out.println(first.doublePredictor());
+        branchAnalysis second = new branchAnalysis(0.3);
+        System.out.println(second.getOdds());
+        System.out.println(second.singlePredictor());
+        System.out.println(second.doublePredictor());
+        branchAnalysis third = new branchAnalysis(0.5);
+        System.out.println(third.getOdds());
+        System.out.println(third.singlePredictor());
+        System.out.println(third.doublePredictor());
+        branchAnalysis fourth = new branchAnalysis(0.7);
+        System.out.println(fourth.getOdds());
+        System.out.println(fourth.singlePredictor());
+        System.out.println(fourth.doublePredictor()); 
+        branchAnalysis fifth = new branchAnalysis(0.9);
+        System.out.println(fifth.getOdds());
+        System.out.println(fifth.singlePredictor());
+        System.out.println(fifth.doublePredictor());      
     }
 }
